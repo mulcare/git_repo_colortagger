c847d88def7a14651cfc413d8a88ce4b1c564a6d
ross mulcare <ross.mulcare@gmail.com>
2016-05-07 23:17:35 -0400

    initial working prototype

---
+++ git_repo_colortagger.fish
@@ -0,0 +1,36 @@
+#! /usr/local/bin/fish
+
+set red 0000000000000000000C00000000000000000000000000000000000000000000
+set repos (find ~/ -name ".git" | sed 's/.git$//')
+
+for repo in $repos
+  set attrs (xattr $repo)
+  
+  if test -n "$attrs"
+    for attr in $attrs
+      if test "$attr" = "com.apple.FinderInfo"
+        set has_finderinfo TRUE
+      end
+    end  
+  
+    if test has_finderinfo
+      set color (xattr -p com.apple.FinderInfo $repo | sed 's/ *00 *//g')
+      if test "$color" = "0C "
+        echo "$repo already marked...skipping."
+      else
+        xattr -wx com.apple.FinderInfo \ $red $repo
+	echo "$repo not marked...marking."
+      end
+
+    else
+      xattr -wx com.apple.FinderInfo \ $red $repo
+      echo "$repo not marked...marking."
+    end
+  
+  else
+    xattr -wx com.apple.FinderInfo \ $red $repo
+    echo "$repo not marked...marking."
+  end
+
+end
+
